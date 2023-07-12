ea889df4a76acd234e616b9afa907aa58b0f7cce
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-12 08:48:09 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [showModal, setShowModal ] = useState(false);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent"> 
+          Parent Component
+          <div>Child Component</div>
+          <button onClick={() => setShowModal(true)}>Show Modal</button>
+          {showModal ? <div>
+            Modal Content
+            <p>This is modal content</p>
+          </div> : ""}
+        </div>
+
     </div>
   )
 }
