15ae31ff8703a7f09665065b3ccf4bee065715f8
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-12 09:03:34 +0000

    four

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,14 @@ const App = () => {
         {/* Do not remove the main div */}
         <div className="parent">
           <h1>Parent Component</h1>
+          <div className="child">
+            <h2>Child Component</h2>
+            <button onClick={() => setShowModal(true)}>Show Modal</button>
+            {showModal ? <>
+              <h3>Modal Content</h3>
+              <p>This is modal content</p>
+            </>: ""}
+          </div>
         </div>
     </div>
   )
